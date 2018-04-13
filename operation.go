0ff5f3b9d80be3dcf9dbe547092b0aa6b4dda82a
nasa9084 <nasa9084@live.jp>
2018-04-13 15:53:35 +0900

    can specify a status code which contains wild card

--- operation.go
+++ operation.go
@@ -5,12 +5,21 @@ import "strconv"
 // SuccessResponse returns a success response object.
 // If there are 2 or more success responses (like created and ok),
 // it's not sure which is returned.
+// If only match the default response or 2XX response, returned status code will be 0.
 func (op *Operation) SuccessResponse() (*Response, int, bool) {
 	if op.Responses == nil {
-		return nil, 0, false
+		return nil, -1, false
 	}
-
+	var defaultResponse *Response
 	for statusStr, resp := range op.Responses {
+		switch statusStr {
+		case "default":
+			defaultResponse = resp
+		case "2XX":
+			defaultResponse = resp
+		case "1XX", "3XX", "4XX", "5XX":
+			continue
+		}
 		statusInt, err := strconv.Atoi(statusStr)
 		if err != nil {
 			continue
@@ -19,5 +28,5 @@ func (op *Operation) SuccessResponse() (*Response, int, bool) {
 			return resp, statusInt, true
 		}
 	}
-	return op.Responses["Default"], 0, false
+	return defaultResponse, 0, false
 }
