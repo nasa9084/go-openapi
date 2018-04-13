880cccb19d1fa5139ab580df75508a24be07369a
nasa9084 <nasa9084@live.jp>
2018-04-13 17:24:25 +0900

    add todo comment

--- operation_test.go
+++ operation_test.go
@@ -3,6 +3,7 @@ package openapi_test
 import "testing"
 
 func TestSuccessResponse(t *testing.T) {
+	// TODO: more test
 	resp, status, ok := doc.Paths["/"].Get.SuccessResponse()
 	if !ok {
 		t.Error("cannot find success response")
