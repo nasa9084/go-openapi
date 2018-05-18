b8e9ee9bde79a40b897602c3b0897d65ee64867f
nasa9084 <nasa9084@live.jp>
2018-05-18 13:07:11 +0900

    header.validate in the darkness

--- header.go
+++ header.go
@@ -1,5 +1,7 @@
 package openapi
 
+import "errors"
+
 // codebeat:disable[TOO_MANY_IVARS]
 
 // Header Object
@@ -19,4 +21,22 @@ type Header struct {
 	Content map[string]*MediaType
 
 	Ref string `yaml:"$ref"`
+}
+
+// Validate the values of Header object.
+func (header Header) Validate() error {
+	validaters := []validater{header.Schema}
+	if v, ok := header.Example.(validater); ok {
+		validaters = append(validaters, v)
+	}
+
+	// example has no validation
+
+	if len(header.Content) > 1 {
+		return errors.New("header.content must only contain one entry")
+	}
+	for _, mediaType := range header.Content {
+		validaters = append(validaters, mediaType)
+	}
+	return validateAll(validaters)
 }
