5e98868209dd8372e081984ff97051f8af9cafe7
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2018-08-14 23:12:22 +0900

    add test for parameter.Validate()

--- parameter_test.go
+++ parameter_test.go
@@ -31,3 +31,17 @@ func testHasDuplicatedParameterTrue(t *testing.T) {
 		t.Error("should return true")
 	}
 }
+
+func TestParameterValidate(t *testing.T) {
+	candidates := []candidate{
+		{"empty", openapi.Parameter{}, true},
+		{"withName", openapi.Parameter{Name: "foo"}, true},
+		{"withName-in", openapi.Parameter{Name: "foo", In: "path"}, true},
+		{"withName-invalidIn", openapi.Parameter{Name: "foo", In: "bar"}, true},
+		{"withName-inPath-notRequired", openapi.Parameter{Name: "foo", In: "path"}, true},
+		{"withName-inPath-required", openapi.Parameter{Name: "foo", In: "path", Required: true}, false},
+		{"allowEmptyValue-notQuery", openapi.Parameter{Name: "foo", In: "header", AllowEmptyValue: true}, true},
+		{"allowEmptyValue-query", openapi.Parameter{Name: "foo", In: "query", AllowEmptyValue: true}, false},
+	}
+	testValidater(t, candidates)
+}
