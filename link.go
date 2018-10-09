1e95686a5b5ebe3a1959197618eff7a72da30a90
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2018-10-09 15:46:10 +0900

    fix validater of link object

--- link.go
+++ link.go
@@ -1,5 +1,7 @@
 package openapi
 
+import "errors"
+
 // codebeat:disable[TOO_MANY_IVARS]
 
 // Link Object
@@ -16,6 +18,9 @@ type Link struct {
 
 // Validate the values of Link object.
 func (link Link) Validate() error {
+	if link.OperationRef != "" && link.OperationID != "" {
+		return errors.New("operationRef and operationId are mutually exclusive")
+	}
 	validaters := []validater{}
 	for _, i := range link.Parameters {
 		if v, ok := i.(validater); ok {
