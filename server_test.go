39b38f506699c497c17b3d368f9581f7e05afc0e
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2018-10-11 16:26:07 +0900

    add example test for server object

--- server_test.go
+++ server_test.go
@@ -1,9 +1,11 @@
 package openapi_test
 
 import (
+	"reflect"
 	"testing"
 
 	openapi "github.com/nasa9084/go-openapi"
+	yaml "gopkg.in/yaml.v2"
 )
 
 func TestServerValidate(t *testing.T) {
@@ -13,4 +15,22 @@ func TestServerValidate(t *testing.T) {
 		{"withURL", openapi.Server{URL: exampleCom}, nil},
 	}
 	testValidater(t, candidates)
+}
+
+func TestServerByExample(t *testing.T) {
+	spec := `url: https://development.gigantic-server.com/v1
+description: Development server`
+	var server openapi.Server
+	if err := yaml.Unmarshal([]byte(spec), &server); err != nil {
+		t.Error(err)
+		return
+	}
+	expect := openapi.Server{
+		URL:         "https://development.gigantic-server.com/v1",
+		Description: "Development server",
+	}
+	if !reflect.DeepEqual(server, expect) {
+		t.Errorf("%+v != %+v", server, expect)
+		return
+	}
 }
