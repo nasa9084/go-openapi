ae740da6cbcc087589a4c181680e3883124f68c2
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2020-05-23 02:41:03 +0900

    fix gomnd

--- validate.go
+++ validate.go
@@ -8,29 +8,27 @@ import (
 func validateURLTemplate(s string) error {
 	var scheme, address, port, path string
 	rest := s
-	splitScheme := strings.SplitN(rest, "://", 2)
-	if len(splitScheme) == 2 {
+
+	if strings.Contains(rest, "://") {
+		splitScheme := strings.SplitN(rest, "://", 2)
 		scheme = splitScheme[0]
 		rest = splitScheme[1]
-	} else {
-		rest = splitScheme[0]
 	}
 
-	splitPath := strings.SplitN(rest, "/", 2)
-	if len(splitPath) == 2 {
+	if strings.Contains(rest, "/") {
+		splitPath := strings.SplitN(rest, "/", 2)
 		rest = splitPath[0]
 		path = splitPath[1]
-	} else {
-		rest = splitPath[0]
 	}
 
-	splitPort := strings.SplitN(rest, ":", 2)
-	if len(splitPort) == 2 {
+	if strings.Contains(rest, ":") {
+		splitPort := strings.SplitN(rest, ":", 2)
 		address = splitPort[0]
 		port = splitPort[1]
 	} else {
-		address = splitPort[0]
+		address = rest
 	}
+
 	scheme = urlTemplateVarRegexp.ReplaceAllLiteralString(scheme, "http")
 	address = urlTemplateVarRegexp.ReplaceAllLiteralString(address, "placeholder")
 	port = urlTemplateVarRegexp.ReplaceAllLiteralString(port, "80")
